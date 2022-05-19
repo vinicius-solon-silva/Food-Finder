ffdfc628eeb58c7739ef79970df3c0d3cc10d2fc
Vinicius Solon Silva <64141214+vinicius-solon-silva@users.noreply.github.com>
2022-05-19 08:17:42 -0300

    Added condition on finder function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import finder
 
 # Funções da tela
 class Interface(MDFloatLayout):
+    dialog = None
+
     # Chama função finder e muda as labels conforme preciso
     def busca(self):
         # Pega o conteúdo dos textfields e coloca em variáveis
@@ -12,11 +14,18 @@ class Interface(MDFloatLayout):
 
         # Chama a função finder e armazena o dicionário em uma variável
         resposta = finder.findARestaurant(comida, local)
-
+       
         # Muda o texto das labels conforme o resultado
-        self.ids.name.text += resposta['name']
-        self.ids.adress.text += resposta['address']
-        self.ids.photo.text += resposta['photo']
+        if type(resposta) == dict:
+            self.ids.resultados.text = "Resultados encontrados:"
+            self.ids.name.text += resposta['name']
+            self.ids.adress.text += resposta['address']
+            self.ids.photo.text += resposta['photo']
+        else:
+            self.ids.resultados.text = "0 Resultados encontrados, tente novamente"
+            self.ids.name.text = ""
+            self.ids.adress.text = ""
+            self.ids.photo.text = ""
 
 # Não sei pra que serve
 class Interface(MDApp):
